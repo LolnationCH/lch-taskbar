77c2d85daf728a48ebaa86941ab3471a8b974ed1
LolnationCH <lolnationCH@gmail.com>
2023-01-16 20:25:32 -0500

    fix the keydown keyup situation

--- lch-keyboard-handler/InterceptKeys.cs
+++ lch-keyboard-handler/InterceptKeys.cs
@@ -7,6 +7,9 @@ class InterceptKeys
 {
   private const int WH_KEYBOARD_LL = 13;
   private const int WM_KEYDOWN = 0x0100;
+  private const int WM_KEYUP = 0x0101;
+  private const int WM_SYSKEYDOWN = 0x0104;
+  private const int WM_SYSKEYUP = 0x0105;
   private static LowLevelKeyboardProc _proc = HookCallback;
   private static IntPtr _hookID = IntPtr.Zero;
 
@@ -40,12 +43,12 @@ class InterceptKeys
   private static IntPtr HookCallback(
       int nCode, IntPtr wParam, IntPtr lParam)
   {
-    if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+    if (nCode >= 0 && (wParam == (IntPtr)WM_KEYDOWN || wParam == (IntPtr)WM_SYSKEYDOWN))
     {
       int vkCode = Marshal.ReadInt32(lParam);
       _keysDown[(Keys)vkCode] = true;
     }
-    else if (nCode >= 0 && wParam == (IntPtr)0x0101)
+    else if (nCode >= 0 && (wParam == (IntPtr)WM_KEYUP || wParam == (IntPtr)WM_SYSKEYUP))
     {
       int vkCode = Marshal.ReadInt32(lParam);
       _keysDown[(Keys)vkCode] = false;
