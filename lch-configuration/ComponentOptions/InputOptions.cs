ac12e89d16931b38376ac510248fcdb99b48c26e
LolnationCH <lolnationCH@gmail.com>
2023-03-04 18:26:13 -0500

    Adding the options and control

---
+++ lch-configuration/ComponentOptions/InputOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace lch_configuration.ComponentOptions
+{
+  public class InputOptions : IComponentOptions
+  {
+    public string Tooltip { get; set; } = "Input";
+    public string? Icon { get; set; }
+    public string CommandString { get; set; } = "cmd.exe";
+    public string? CommandOptions { get; set; }
+    public int MinWidth { get; set; } = 100;
+  }
+}
--- lch-taskbar-wpf/Utils/ControlsUtils.cs
+++ lch-taskbar-wpf/Utils/ControlsUtils.cs
@@ -1,10 +1,16 @@
-﻿using lch_configuration.Configuration;
+﻿using lch_configuration.ComponentOptions;
+using lch_configuration.Configuration;
+using lch_taskbar.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Interop;
+using System.Windows.Media.Imaging;
 
 namespace lch_taskbar_wpf.Utils
 {
@@ -19,6 +25,17 @@ namespace lch_taskbar_wpf.Utils
         TaskbarPosition.Top    => Orientation.Horizontal,
         TaskbarPosition.Bottom => Orientation.Horizontal,
         _ => Orientation.Horizontal
+      };
+    }
+
+    public static Image GetImageFromImagePath(string Path, string ToolTip)
+    {
+      return new Image()
+      {
+        MaxWidth = 25,
+        MaxHeight = 25,
+        ToolTip = ToolTip,
+        Source = new BitmapImage(new Uri(Path)),
       };
     }
   }
